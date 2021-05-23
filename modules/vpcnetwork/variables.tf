3240456c2efab1d97d89710570d951590868d421
shaikh fauzia naushad <shaikhfauzia.naushad@publicissapient.com>
2021-05-23 16:11:12 +0530

    VPC and Subnets

---
+++ modules/vpcnetwork/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "The project ID"
+  default     = "burner-shanaush"
+}
+variable "region" {
+  description = "The region of the project."
+  default     = "us-central1"
+}
+variable "zone" {
+  description = "The zone of the project."
+  default     = "us-central1-a"
+}
+variable "network_name" {
+  description = "The network"
+  default     = "vpc-network"
+}
